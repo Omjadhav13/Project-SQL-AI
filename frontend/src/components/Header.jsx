3d565718d5bc0b0f6b4551788c6bc93a00fcc66a
Omjadhav13 <omjadhav1303@gmail.com>
2026-06-22 20:41:50 +0530

    Added components

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-function Header() {
+function Header(props) {
   return (
     <div>
         <h1>SQL Query Analyzer</h1>
         <hr />
         <h3>Understand how SQL works internally</h3>
+
+        <h4>Hello {props.setName}</h4>
     </div>
   )
 }
