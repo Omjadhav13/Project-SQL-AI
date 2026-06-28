7bc6c71969d323d45c900a57724def382307588b
Omjadhav13 <omjadhav1303@gmail.com>
2026-06-28 15:01:41 +0530

    Cors File

---
+++ backend/sql-analyzer/src/main/java/com/sqlpro/sql_analyzer/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.sqlpro.sql_analyzer.config;
+
+import org.springframework.boot.webmvc.autoconfigure.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+	@Bean
+	public WebMvcConfigurer  corConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("https://localhots/5173")
+						.allowedMethods("*");
+			}
+		};
+	}
+}
