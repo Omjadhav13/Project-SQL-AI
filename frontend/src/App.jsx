f2300e46f1c35821f29c04873ece1791bc97dec9
Omjadhav13 <omjadhav1303@gmail.com>
2026-06-28 15:00:29 +0530

    Full code ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,22 +3,63 @@ import AnalyzeButton from './components/AnalyzeButton';
 import Header from './components/Header';
 import ResultCard from './components/ResultCard';
 import SqlInput from './components/SqlInput';
-import React,{useState} from 'react';
+import React, { useState } from 'react';
+import { analyzeQuery } from './api/analyzerApi';
 
 function App() {
   const [query, setQuery] = useState("");
-  const [analyzedQuery, setAnalyzedQuery] = useState("");
-  const handleAnalyze=()=>{
-    setAnalyzedQuery(query);
+  const [analyzedResult, setAnalyzedResult] = useState(null);
+  const [Loading, setLoading] = useState(false);
+  const [Error, setError] = useState("");
+  const handleAnalyze = async () => {
+    setLoading(true);
+    setError("");
+    try {
+      const result = await analyzeQuery(query);
+      setAnalyzedResult(result);
+    }
+    catch (e) {
+      console.log(e.message);
+    }
+    finally {
+      setLoading(false);
+    }
+
   }
   return (
     <div>
       <Header />
-      <SqlInput query={query} setQuery={setQuery}/>
-      <AnalyzeButton onAnalyze={handleAnalyze}/>
-      <ResultCard title="Anlayzed Query" content={analyzedQuery}/>
-    </div>
-  )
-}
+      <SqlInput query={query} setQuery={setQuery} />
+      <AnalyzeButton onAnalyze={handleAnalyze} />
+      {loading && <h2>Analyzing...</h2>}
 
+      {error && <p>{error}</p>}
+
+      {analysisResult && (
+        <>
+          <ResultCard
+            title="Query Type"
+            content={analysisResult.queryType}
+          />
+          <ResultCard
+            title="Parsing"
+            content={analysisResult.parsing}
+          />
+          <ResultCard
+            title="Optimization"
+            content={analysisResult.optimization}
+          />
+          <ResultCard
+            title="Execution"
+            content={analysisResult.execution}
+          />
+          <ResultCard
+            title="Result Generation"
+            content={analysisResult.resultGeneration}
+          />
+        </>
+      )}
+    </div>
+  );
+}
 export default App;
---
+++ frontend/src/api/analyzerApi.js
@@ -0,0 +1,12 @@
+import { axios } from "axios";
+
+const API_URL= "http://localhost:8080/analyze";
+
+export const analyzeQuery = async (query)=>{
+    const response =await  axios.post(
+        API_URL,{
+            query: query
+        }
+    );
+    return response.data;
+}
