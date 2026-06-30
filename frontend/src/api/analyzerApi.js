ae57f8a503d574d08cfc312850723b83f61f66a3
Omjadhav13 <omjadhav1303@gmail.com>
2026-06-30 21:05:27 +0530

    Connection File updated

--- frontend/src/api/analyzerApi.js
+++ frontend/src/api/analyzerApi.js
@@ -1,6 +1,6 @@
 import  axios  from "axios";
 
-const API_URL= "http://localhost:8080/analyze";
+const API_URL= "https://project-sql-ai.onrender.com/analyze";
 
 export const analyzeQuery = async (query)=>{
     const response =await  axios.post(
